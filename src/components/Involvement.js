7b352ef4daf7f258b75dcb931bbd098638fdd701
gperera89 <80236223+gperera89@users.noreply.github.com>
2022-06-13 19:04:43 +0800

    fixed typos

--- src/components/Involvement.js
+++ src/components/Involvement.js
@@ -7,7 +7,7 @@ const Involvement = ({ involementRating, name, pronoun }) => {
 		case 2:
 			return `In lessons, ${pronoun[0]} is usually quiet, but will make thoughtful contributions when called upon. `;
 		case 3:
-			return ` In lessons, ${pronoun[0]} is good-humoured and makes frequent contributions to class discussions and but hesitates to ask questions to clarify ${pronoun[2]} understanding. `;
+			return ` In lessons, ${pronoun[0]} is good-humoured and makes frequent contributions to class discussions but hesitates to ask questions to clarify ${pronoun[2]} understanding. `;
 		case 4:
 			return `In lessons, ${pronoun[0]} makes frequent contributions to class discussions and will regularly ask questions to develop a better understanding. `;
 		case 5:
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -17,7 +17,7 @@ const Summary = ({ improvement, name, pronoun }) => {
 		case "Resources":
 			return ` ${name} would benefit greatly from more regularly reviewing the material using the range of resources developed to support ${pronoun[2]}. With better utilisation of ${pronoun[2]} revision resources, ${name} will continue to progress in Science.`;
 		case "Speaking in Class":
-			return `${name} should aim to develop ${pronoun[2]} articulation of scientific ideas further, as this will extend ${pronoun[2]} understanding as well as build skills which will be crucial in ${pronoun[2]} future. With more involvement in class, ${name} will continue to make progress in Science.`;
+			return `${name} should aim to develop ${pronoun[2]} articulation of scientific ideas further, as this will extend ${pronoun[2]} understanding and build skills which will be crucial in ${pronoun[2]} future. With more involvement in class, ${name} will continue to make progress in Science.`;
 		default:
 			return "";
 	}
